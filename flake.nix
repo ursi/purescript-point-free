3f348bd8e3845f58c108557471183d1adac4fe5f
Mason Mackaman <masondeanm@aol.com>
2020-12-11 16:30:21 -0500

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,13 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        with nixpkgs.legacyPackages.${system};
-
-        {
-          devShell = mkShell {
-            buildInputs = [
-              dhall
-              nodejs
-              nodePackages.bower
-              nodePackages.pulp
-              purescript
-              spago
-            ];
-          };
-        }
-      );
+  outputs = { self, nixpkgs, utils }:
+    utils.simpleShell
+      [
+        "dhall"
+        "nodejs"
+        "nodePackages.bower"
+        "nodePackages.pulp"
+        "purescript"
+        "spago"
+      ]
+      nixpkgs;
 }
